f6fe6cff98ada70e0f361cced13abf3b9ccd92fe
JabelResendiz <jabelresendiz18@gmail.com>
2024-06-10 23:24:42 -0400

    modified Prim algorithm

--- Graph/prim.cpp
+++ Graph/prim.cpp
@@ -3,24 +3,65 @@
 
 using namespace std;
 
-const int INF = 1000000000;
 
-struct Edges
+
+/*
+
+
+Graph: 
+adj.size()=2 , el primer argumento es la arista con la que esta conectada y el segundo parametro es el peso de esa arista
+
+Ejemplo:
+
+
+vector<vector<int>>adj[]=
 {
-    int w = INF, to = -1;
-    bool operator<(Edges const &other) const
-    {
-        return make_pair(w, to) < make_pair(other.w, other.to);
-    }
-};
+{ {1,10} , {2,5} }, // el vertice 0 unido a los vertices 1y 2 con pesos 10 y 5
+{ {0,10},  {2,2} },// el vertice 1 unido a los vertices 0 y 2 con pesos 10 y 2
+{ {0,5},  {1,2}  } // el vertice 2 unido a los vertices 0 y 1 con pesos 5 y 2
+}
+
+*/
 
 class Prim
 {
 public:
-   
-
-    void prim( int n, vector<vector<Edges>> adj)
+    int prim(int n, vector<vector<int>> adj[])
     {
         priority_queue<pair<int, int>> q;
+
+        vector<bool> mask;
+        mask.assign(n, false);
+        mask[0] = true;
+
+        int cost = 0;
+
+        for (int i = 0; i < adj[0].size(); i++)
+        {
+            q.push({-adj[0][i][1], adj[0][i][0]});
+        }
+
+        while (q.size() != 0)
+        {
+            auto aux = q.top();
+            q.pop();
+
+            int k = aux.second;
+            if (mask[k])
+                continue;
+
+            mask[k] = true;
+            cost += abs(aux.first);
+
+            for (int i = 0; i < adj[k].size(); i++)
+            {
+                if (!mask[adj[k][i][0]])
+                {
+                    q.push({-adj[k][i][1], adj[k][i][0]});
+                }
+            }
+        }
+
+        return cost;
     }
 };
