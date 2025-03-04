6ac170f337faba7f0c059c4e9a0830f2068e1a0a
JabelResendiz <jabelresendiz18@gmail.com>
2025-03-03 23:56:47 -0500

    manhattan_distance

---
+++ Manhattan_Distance/Manhattan.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// d(p,q)= |xp - xq| + |yp - yq|
+// max [(xp + (-xq)) + (yp + (-yq))] = max (xp + yp) + max(-xq-yq)
+// who
+
+
+long long FarthestPairManhattanDistance(int d,int n, vector<vector<int>>p)
+{
+    long long ans =0;
+
+    for (int msk = 0; msk < (1 << d); msk++) {
+        long long mx = LLONG_MIN, mn = LLONG_MAX;
+        for (int i = 0; i < n; i++) {
+            long long cur = 0;
+            for (int j = 0; j < d; j++) {
+                if (msk & (1 << j)) cur += p[i][j];
+                else cur -= p[i][j];
+            }
+            mx = max(mx, cur);
+            mn = min(mn, cur);
+        }
+        ans = max(ans, mx - mn);
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+}
