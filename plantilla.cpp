724c6d921b1b3bbc23a102cfa730d4797b9320cf
JabelResendiz <jabelresendiz18@gmail.com>
2025-03-09 23:37:12 -0400

    manhattan with segment tree

---
+++ plantilla.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define vi vector<int>
+#define vii vector<vi>
+#define vll vector<ll>
+#define all(x) (x).begin(), (x).end()
+#define forn(i,a,n) for (int i=int(a);i<int(n);i++)
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+ 
+}
