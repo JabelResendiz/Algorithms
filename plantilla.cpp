df6917c87ee7476f51ddb01bf00d59844d4309de
JabelResendiz <jabelresendiz18@gmail.com>
2025-09-08 10:03:56 -0600

    update template cpp

--- plantilla.cpp
+++ plantilla.cpp
@@ -1,18 +1,72 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
-#define vi vector<int>
-#define vii vector<vi>
-#define vll vector<ll>
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+
 #define all(x) (x).begin(), (x).end()
 #define forn(i,a,n) for (int i=int(a);i<int(n);i++)
+#define CIN(...) input(__VA_ARGS__)
+
+template<typename T>
+void input(T &x) {cin >> x};
+
+template<typename T, typename... Args>
+void input(T &x, Args&... args)
+{
+    cin >> x ;
+    input(args...);
+}
+
+#define COUT(...) ouput(__VA_ARGS__)
+
+template<typename T>
+void output(T&x) {cout << x <<endl;}
+
+template<typename T, typename... Args>
+void output(T&x, Args&...args)
+{
+    cout << x << " ";
+    output(args...);
+}
+
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+
+void solve()
+{
+
+    
+
+}
+
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
 
- 
+    ios::sync_with_stdio(false);
+    int t;
+    cin >>t;
+
+    while(t--) solve();
+    
+    return 0;
+    
 }
